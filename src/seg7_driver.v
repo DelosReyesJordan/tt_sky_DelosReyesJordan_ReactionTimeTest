f9b6a2344485e35b9e5514158abdeb6fc10ddb07
DelosReyesJordan <63662211+DelosReyesJordan@users.noreply.github.com>
2025-08-24 20:26:23 -0700

    Update seg7_driver.v

--- src/seg7_driver.v
+++ src/seg7_driver.v
@@ -28,10 +28,10 @@ module seg7_driver(
 
     wire [3:0] digits [3:0];
 
-    assign digits[3] = ((value / 1000) % 10)[3:0];
-    assign digits[2] = ((value / 100)  % 10)[3:0];
-    assign digits[1] = ((value / 10)   % 10)[3:0];
-    assign digits[0] = ( value          % 10)[3:0];
+    assign digits[3] = ((value / 1000) % 10) & 4'hF;
+    assign digits[2] = ((value / 100)  % 10) & 4'hF;
+    assign digits[1] = ((value / 10)   % 10) & 4'hF;
+    assign digits[0] = ( value         % 10) & 4'hF;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
