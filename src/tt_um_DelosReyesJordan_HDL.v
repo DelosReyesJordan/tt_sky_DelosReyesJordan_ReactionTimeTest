f49e01eed703c6dfae58e9812ef2a87b6e8afc83
DelosReyesJordan <63662211+DelosReyesJordan@users.noreply.github.com>
2025-08-24 23:47:23 -0700

    Update tt_um_DelosReyesJordan_HDL.v

--- src/tt_um_DelosReyesJordan_HDL.v
+++ src/tt_um_DelosReyesJordan_HDL.v
@@ -2,8 +2,8 @@ module tt_um_DelosReyesJordan_HDL (
     input  wire [7:0] ui_in,     // general inputs (buttons)
     output wire [7:0] uo_out,    // segments and LED
     input  wire [7:0] uio_in,    // not used
-    output wire [3:0] uio_out,   // anode signals
-    output wire [3:0] uio_oe,    // anode enable signals
+    output wire [7:0] uio_out,   // anode signals
+    output wire [7:0] uio_oe,    // anode enable signals
     input  wire       ena,       // always 1
     input  wire       clk,       // system clock
     input  wire       rst_n      // active-low reset
@@ -20,8 +20,8 @@ module tt_um_DelosReyesJordan_HDL (
     reaction_time_top dut (
         .ui_in(ui_in),
         .uo_out(uo_out),
-        .uio_out({4'b0000, uio_out}), // map lower 4 bits to 7-segment anodes
-        .uio_oe({4'b0000, uio_oe}),   // map lower 4 bits to anode enables
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),  // <- now must be 8 bits wide inside dut too
         .clk(clk),
         .rst_n(rst_n)
     );
@@ -30,3 +30,4 @@ module tt_um_DelosReyesJordan_HDL (
     wire unused = (&uio_in) & ena;
 
 endmodule
+
